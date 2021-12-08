d5f0ff88c2b1078cc56b81211adefbb02d1fb8bf
Jonnathan Garces <jonnathangarces@Jonnathans-MacBook-Pro.local>
2021-12-08 00:24:15 -0500

    aapi data retrieved

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// GETTING SOME DATA
+
+const getData = async() => {
+    let name = document.querySelector('.search-bar').value;
+    console.log(name)
+    document.querySelector('.search-bar').value= ''
+    let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    let pokemon = res.data;
+    console.log(pokemon);
+    return pokemon
+};
+
+const loadData = async() => {
+    let pokemonData = await getData();
+
+    // finding name
+    let name = pokemonData.name;
+    let photo = pokemonData.sprites.back_default;
+    
+
+    // getting card elements
+    let photo_area = document.getElementsByTagName('img')[0];
+    photo_area.src = photo
+} 
